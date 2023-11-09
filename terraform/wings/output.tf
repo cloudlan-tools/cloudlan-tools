0448f4bded3d1796796da0798331adbe04c4d027
The0Mikkel <mikkel.alb@gmail.com>
2023-11-09 22:01:05 +0100

    Added outputs

--- terraform/wings/output.tf
+++ terraform/wings/output.tf
@@ -1,1 +1,10 @@
-#
+# IP address of the Hetzner Cloud server
+output "ip_address" {
+  value = module.wing-node.ip_address
+}
+
+# SSH key for the Hetzner Cloud server
+output "ssh_private_key" {
+  value     = module.wing-node.ssh_private_key
+  sensitive = true
+}
