742221e7ce5b4576a669c71b55715345c4038f14
William Bergmann Børresen <wbb@netcompany.com>
2023-11-09 20:46:47 +0100

    appended to .gitignore

--- terraform/wings/providers.tf
+++ terraform/wings/providers.tf
@@ -1,13 +1,26 @@
 terraform {
+  #Required terraform version
+  required_version = ">= 1.6.2"
+  # Required providers
   required_providers {
+  # Hetzner Cloud Provider
     hcloud = {
       source = "hetznercloud/hcloud"
+      version = ">=1.44.1"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = ">=4.18.0"
     }
   }
-  required_version = ">= 1"
 }
 
 # Configure the Hetzner Cloud Provider
 provider "hcloud" {
   token = var.hcloud_token
+}
+
+# Configure the Cloudflare Provider
+provider "cloudflare" {
+  api_key = var.cloudflare_api_token
 }
--- terraform/wings/variables.tf
+++ terraform/wings/variables.tf
@@ -2,3 +2,8 @@ variable "hcloud_token" {
   sensitive = true # Requires terraform >= 0.14
   type = string
 }
+
+variable "cloudflare_api_token" {
+  sensitive = true # Requires terraform >= 0.14
+  type = string
+}
